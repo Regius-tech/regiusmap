e8b576366979ac3ca53a05a34ba626956523ebca
Regius Tech <mikael.h.nore@outlook.com>
2025-10-21 15:48:09 +0200

    2

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,10 @@
 // firebaseConfig.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyCKqwpql2Yl0kbpUIPrQUYyVd7m1OeH-D8",
+  apiKey: "AIzaSyCKqwpql2Yl0kbpUIPrQUYyAjd1JlMvNg",
   authDomain: "triflex-a08c7.firebaseapp.com",
-  databaseURL: "https://triflex-a08c7-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "triflex-a08c7",
   storageBucket: "triflex-a08c7.firebasestorage.app",
   messagingSenderId: "835381689765",
   appId: "1:835381689765:web:e0be4b22e5f35ca1e0bc4c",
-  measurementId: "G-0WJP4SRVYK"
+  databaseURL: "https://triflex-a08c7-default-rtdb.europe-west1.firebasedatabase.app"
 };
